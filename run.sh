e75244fb492f33836c02d8a665b7a8dbbd71f6a1
Nick Nystrom <nnystrom@gmail.com>
2023-01-02 12:47:31 -0600

    Provision mysql/postgres shared dbs

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 TASK=${TASK:-backup}
 
-while getopts "br:d:m" option; do
+while getopts "br:d:mp" option; do
     case $option in
         b)
             TASK=backup
@@ -34,6 +34,10 @@ export PGDATABASE=$DB_NAME
 export PGUSER=${DB_USERNAME:-$DB_NAME}
 export PGPASSWORD=${DB_PASSWORD:-$PGUSER}
 
+export ROOT_USER=${ROOT_USER:-root}
+export ROOT_PASSWORD=${ROOT_PASSWORD:-unknown}
+
+
 backup() {
     D=`date +%Y-%m-%d`
     FN=$DB_NAME-$D.dump
@@ -45,6 +49,24 @@ backup() {
 restore() {
     s3cmd --access_key=$AWS_S3_ACCESS_KEY_ID --secret_key=$AWS_S3_SECRET_ACCESS_KEY --force get s3://$AWS_STORAGE_BUCKET_NAME/$FN
     pg_restore --clean --no-owner --no-privileges -vvv -d $PGDATABASE "$FN" || mysql --host=$PGHOST --port=$MYSQL_PORT --user=$PGUSER --password=$PGPASSWORD $DB_NAME < "$FN"
+}
+
+provision_mysql() {
+    mysql --host=$PGHOST --port=$MYSQL_PORT --user=$ROOT_USER --password=$ROOT_PASSWORD <<- EOF
+    create database if not exists \`$PGDATABASE\`;
+    create user if not exists \`$PGUSER\` identified by '$PGPASSWORD';
+    grant all on \`$PGDATABASE\`.* to \`$PGUSER\`;
+EOF
+}
+
+provision_postgres() {
+    export NEW_USER=${PGUSER}
+    export NEW_PASSWORD=${PGPASSWORD}
+    export PGPASSWORD=${ROOT_PASSWORD}
+    export PGUSER=${ROOT_USER}
+    export PGDATABASE=
+    psql -tc "SELECT 1 FROM pg_user WHERE usename = '$NEW_USER'" | grep -q 1 || psql -c "create role \"$NEW_USER\" login password '$NEW_PASSWORD';"
+    psql -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1 || psql -c "create database \"$DB_NAME\" with owner '$NEW_USER';"
 }
 
 case $TASK in
@@ -59,6 +81,12 @@ case $TASK in
         ;;
     psql)
         psql
+        ;;
+    provision_mysql)
+        provision_mysql
+        ;;
+    provision_postgres)
+        provision_postgres
         ;;
     *)
         echo "Usage: $0 {-b|-r file_name} [-d db_name]"
